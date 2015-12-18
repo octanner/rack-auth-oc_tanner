cc2f51abb9b256559851068f40e0213b5941b779
Jay Wagnon <jaywagnon@gmail.com>
2015-12-18 15:06:36 -0700

    Update spec for current rspec standards and practices.

--- spec/rack-auth-oc_tanner_spec.rb
+++ spec/rack-auth-oc_tanner_spec.rb
@@ -2,17 +2,28 @@ require 'spec_helper'
 
 describe Rack::Auth::OCTanner do
 
+  scope_list = ["public", "user.read", "user.write", "user.delete", "admin.read", "admin.write", "admin.delete"]
   before :each do
-    ENV.stub(:[]).with("SCOPES").and_return("public, user.read, user.write, user.delete, admin.read, admin.write, admin.delete")
+    allow(ENV).to receive(:[]).with("SCOPES").and_return("public, user.read, user.write, user.delete, admin.read, admin.write, admin.delete")
   end
 
   subject(:octanner){ Rack::Auth::OCTanner }
 
   describe '.scopes' do
     subject{ octanner.scopes }
-    it{ should be }
-    it{ should be_kind_of Rack::Auth::OCTanner::ScopeList }
-    its(:size){ should be 7 }
+    it{ is_expected.to be }
+    it{ is_expected.to be_kind_of Rack::Auth::OCTanner::ScopeList }
+
+    it "should have 7 scopes" do
+      expect(subject.size).to be(7)
+    end
+    context "has scopes" do
+      scope_list.each do |scope|
+        it "#{scope}" do
+          expect(subject.has_scope?(scope)).to be(true)
+        end
+      end
+    end
   end
 
 end
